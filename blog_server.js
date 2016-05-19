0572e4a7e3422deeb5ff69343eff52089a64481a
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-05-19 08:48:59 +0300

    Change commenting

--- blog_server.js
+++ blog_server.js
@@ -5,20 +5,20 @@ const express      = require('express');
 const bodyParser   = require('body-parser');
 const config  = require('./knexfile.js');
 
+// Initialize Express.
 const app = express();
 app.use(bodyParser.json());
 
+// Determine server environment (development|testing).
 const dbName = process.env.NODE_ENV ? 'learnco_blog_test' : 'learnco_blog';
 console.log('Using database: ', dbName);
 
-// ***** Knex & Bookshelf Configuration ***** //
-
+// Configure & Initialize Bookshelf & Knex.
 console.log('Running in environment: ' + process.env.NODE_ENV);
 const knex = require('knex')(config[process.env.NODE_ENV]);
 const bookshelf = require('bookshelf')(knex);
 
-// ***** Module Exports ***** //
-
+// Exports for Server hoisting.
 const listen = (port) => {
   return new Promise((resolve, reject) => {
     app.listen(port, () => {
