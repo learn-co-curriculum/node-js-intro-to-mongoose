bfa980fa57efdbca97253381d23c0d6193484470
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-05-19 09:15:12 +0300

    Change commenting

--- blog_server.js
+++ blog_server.js
@@ -5,15 +5,13 @@ const express      = require('express');
 const bodyParser   = require('body-parser');
 const config  = require('./knexfile.js');
 
+// Initialize Express.
 const app = express();
 app.use(bodyParser.json());
 
-// ***** Knex & Bookshelf Configuration ***** //
-
+// Configure & Initialize Bookshelf & Knex.
 console.log('Running in environment: ' + process.env.NODE_ENV);
-
 const knex = require('knex')(config[process.env.NODE_ENV]);
-
 const bookshelf = require('bookshelf')(knex);
 
 // ***** Models ***** //
@@ -144,7 +142,7 @@ app.post('/comment', (req, res) => {
     });
 });
 
-// ***** Module Exports ***** //
+// Exports for Server Hoisting.
 
 const listen = (port) => {
   return new Promise((resolve, reject) => {
